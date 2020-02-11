2a268efcd50fe47d5c924184b404a1071d2aafdc
Neel <neel2292@gmail.com>
2020-02-11 00:30:47 +0000

    Main pages changes

--- namlifa_portal/www/member/index.py
+++ namlifa_portal/www/member/index.py
@@ -9,9 +9,5 @@ def get_context(context):
     context.user = frappe.session.user
     context.user_doc = frappe.session
     context.csrf_token = frappe.sessions.get_csrf_token()
-    context.emp_doc = frappe.get_list('Employee',
-                                      { 'company_email': frappe.session.user },
-                                      ['name', 'company', 'date_of_joining', 'branch']
-                                      )
 
     return context
