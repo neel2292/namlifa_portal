fbc0a26ef5b96a2410bff6c9b0d9749f41607c7e
frappe <frappe@ubuntu.vm>
2020-02-19 15:50:52 +0000

    display card on desk for namlifa portal

--- namlifa_portal/config/desktop.py
+++ namlifa_portal/config/desktop.py
@@ -4,38 +4,13 @@ from frappe import _
 
 def get_data():
 	return [
-		{
-			"module_name": "Namlifa Members",
-			"color": "red",
-			"icon": "octicon octicon-organization",
-			"type": "module",
-			"label": _("Namlifa Members"),
-			"hidden": 1
-		},
-		{
-			"module_name": "Namlifa Member",
-			"color": "red",
-			"icon": "octicon octicon-person",
-			"_doctype": "Namlifa Member",
-			"type": "list",
-			"link": "List/Namlifa Member",
-			"label": _("Namlifa Member")
-		},
-		{
-			"module_name": "Namlifa Akard",
-			"color": "red",
-			"icon": "octicon octicon-organization",
-			"type": "module",
-			"label": _("Namlifa Akard"),
-			"hidden": 1
-		},
-		{
-			"module_name": "Namlifa Akard",
-			"color": "red",
-			"icon": "octicon octicon-person",
-			"_doctype": "Namlifa Akard",
-			"type": "list",
-			"link": "List/Namlifa Akard",
-			"label": _("Namlifa Akard")
-		}
+                {
+                        "module_name": "Namlifa Portal",
+                        "category": "Modules",
+                        "label": _("Namlifa Portal"),
+                        "color": "#FF888B",
+                        "icon": "octicon octicon-organization",
+                        "type": "module",
+                        "description": "Namlifa Portal."
+                }
 	]
