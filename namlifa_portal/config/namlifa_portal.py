21c080b6ff0e9b61cf68149719c41c60f809a203
frappe <frappe@ubuntu.vm>
2020-02-19 16:05:42 +0000

    set items related to apps

--- namlifa_portal/config/namlifa_portal.py
+++ namlifa_portal/config/namlifa_portal.py
@@ -4,91 +4,23 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"label": _("Issues"),
+			"label": _("Membership Management"),
 			"items": [
+
+                                {
+                                        "type": "doctype",
+                                        "name": "Namlifa Member",
+                                },      
+                                {
+                                        "type": "doctype",
+                                        "name": "Namlifa Akard",
+                                },
+
+
 				{
 					"type": "doctype",
 					"name": "Issue",
-					"description": _("Support queries from customers."),
 					"onboard": 1,
-				},
-				{
-					"type": "doctype",
-					"name": "Communication",
-					"description": _("Communication log."),
-					"onboard": 1,
-				},
-			]
-		},
-		{
-			"label": _("Warranty"),
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Warranty Claim",
-					"description": _("Warranty Claim against Serial No."),
-				},
-				{
-					"type": "doctype",
-					"name": "Serial No",
-					"description": _("Single unit of an Item."),
-				},
-			]
-		},
-		{
-			"label": _("Service Level Agreement"),
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Employee Group",
-					"description": _("Support Team."),
-				},
-				{
-					"type": "doctype",
-					"name": "Service Level",
-					"description": _("Service Level."),
-				},
-				{
-					"type": "doctype",
-					"name": "Service Level Agreement",
-					"description": _("Service Level Agreement."),
-				}
-			]
-		},
-		{
-			"label": _("Maintenance"),
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Maintenance Schedule",
-				},
-				{
-					"type": "doctype",
-					"name": "Maintenance Visit",
-				},
-			]
-		},
-		{
-			"label": _("Reports"),
-			"icon": "fa fa-list",
-			"items": [
-				{
-					"type": "page",
-					"name": "support-analytics",
-					"label": _("Support Analytics"),
-					"icon": "fa fa-bar-chart"
-				},
-				{
-					"type": "report",
-					"name": "Minutes to First Response for Issues",
-					"doctype": "Issue",
-					"is_query_report": True
-				},
-				{
-					"type": "report",
-					"name": "Support Hours",
-					"doctype": "Issue",
-					"is_query_report": True
 				},
 			]
 		},
