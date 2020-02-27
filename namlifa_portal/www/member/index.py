dd598c88c132727d3b96e0f917548915117dc936
Neel <neel2292@gmail.com>
2020-02-27 15:59:00 +0000

    front and backend update

--- namlifa_portal/www/member/index.py
+++ namlifa_portal/www/member/index.py
@@ -6,8 +6,23 @@ def get_context(context):
         frappe.local.flags.redirect_location = '/'
         raise frappe.Redirect
 
+    member = frappe.db.get_value("Namlifa Member", {"email": frappe.session.user}, "*")
+    akard = frappe.db.get_value("Namlifa Akard", {"namlifa_member_id": member.name}, "*")
+    # pa = frappe.db.get_value("Namlifa PA", {"membership_no": membership_no}, "*")
+    # pa_member = frappe.db.get_values("Namlifa PA Member", {"parent": pa.name}, "*")
+    # pi = frappe.db.get_value("Namlifa PI", {"membership_no": member.name}, "*")
+    # pi_member = frappe.db.get_values("Namlifa PI Member", {"parent": pi.name}, "*")
+    # training = frappe.db.get_value("Namlifa Akard", {"email": frappe.session.user}, "*")
+
     context.user = frappe.session.user
     context.user_doc = frappe.session
     context.csrf_token = frappe.sessions.get_csrf_token()
+    context.member = member
+    context.akard = akard
+    # context.training = training
+    # context.pa = pa
+    # context.pa_members = pa_member
+    # context.pi = pi
+    # context.pi_members = pi_member
 
     return context
